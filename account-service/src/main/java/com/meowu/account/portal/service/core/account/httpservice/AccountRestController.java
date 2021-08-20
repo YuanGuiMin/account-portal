31e146e09b165f336a92f1227e8f9d1ccd07c101
lihaihua <zhangbo>
2021-08-20 15:47:30 +0800

    account login and register

---
+++ account-service/src/main/java/com/meowu/account/portal/service/core/account/httpservice/AccountRestController.java
@@ -0,0 +1,34 @@
+package com.meowu.account.portal.service.core.account.httpservice;
+
+import com.meowu.account.portal.client.account.entity.view.AccountVO;
+import com.meowu.account.portal.service.core.account.service.AccountService;
+import com.meowu.commons.utils.security.response.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.MessageFormat;
+
+@RestController
+@RequestMapping(value = "/api/1.0/account")
+public class AccountRestController{
+
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response register(String username, String password){
+        accountService.register(username, password);
+
+        return new Response(MessageFormat.format("account[{0}] register successfully", username));
+    }
+
+    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response<AccountVO> login(String username, String password){
+        AccountVO view = accountService.login(username, password);
+
+        return new Response<AccountVO>("account login successfully", view);
+    }
+}
